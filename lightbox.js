618582c00b14fbf9b9fa6eae055f6033db7febae
trystero49 <peter.angelinos@gmail.com>
2016-02-02 11:33:04 -0500

    maybe

--- lightbox.js
+++ lightbox.js
@@ -1,6 +1,7 @@
 $('a img').click(function () {
     var $img = $(this);
-    $('#divLargerImage').html($img.clone().height(250).width(250)).add($('#divOverlay')).fadeIn();
+    
+$('#divLargerImage').html($img.clone().height(250).width(250)).add($('#divOverlay')).fadeIn();
 });
 
 $('#divLargerImage').add($('#divOverlay')).click(function () {
